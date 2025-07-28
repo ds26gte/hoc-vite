a60c8d7c2af1dc0f4f99bed229a65076ac1d6fb4
Dorai Sitaram <ds26gte@gmail.com>
2025-07-28 10:32:51 -0400

    clean up images/ videos/ subdirs

--- src/hocBookBits.jsx
+++ src/hocBookBits.jsx
@@ -341,7 +341,7 @@ translate(pupil, 10, 30, eye-white)
             `
     },
 
-    { videoConfig: "images/eye.mov",
+    { videoConfig: "videos/eye.mov",
       lessonText: `
           <h1>13. Full Pyret</h1>
 
